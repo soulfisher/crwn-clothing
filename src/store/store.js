eb146e2626ac3f3a34cde24a2b5fc297da8a05e4
Cassandra Cook <cassandracook@gmail.com>
2026-01-11 09:38:33 -0800

    Redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 //import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
@@ -16,6 +18,14 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
   console.log("next state: ", store.getState());
 };
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [loggerMiddleware];
 
